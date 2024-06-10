00c645734d90680caab50c4ee6326e0b31b3f26c
Sarasmishra <Sarasmishra8100@gmail.com>
2024-06-10 09:25:22 +0530

    latest commit

---
+++ src/components/StopWatch.jsx
@@ -0,0 +1,37 @@
+// src/components/Stopwatch.jsx
+
+import { useState, useEffect } from "react";
+
+function StopWatch() {
+  const [count, setCount] = useState(0);
+
+  useEffect(() => {
+    const IntervalId = setInterval(()=>{
+      setCount((prevcount)=>{
+
+        if(prevcount>=9){
+          clearInterval(IntervalId)
+        }
+
+        return prevcount +1;
+      })
+
+    },1000)
+
+    function CleanUpFunction(){
+      clearInterval(IntervalId)
+    }
+
+    return CleanUpFunction;
+
+  }, []);
+
+  return (
+    <div style={{ border: "1px dashed red", padding: "10px", margin: "10px" }}>
+      <h1>Stop Watch</h1>
+      <h4>{count}</h4>
+    </div>
+  );
+}
+
+export default StopWatch;
